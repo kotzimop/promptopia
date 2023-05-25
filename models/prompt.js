84120beff62433160c8fc9b82be697b9372c55b4
kotzimop <kotzimop@gmail.com>
2023-05-25 18:04:31 +0300

    Create page & Form and backend to store to DB

---
+++ models/prompt.js
@@ -0,0 +1,22 @@
+import  {Schema, model, models} from "mongoose";
+
+const promptSchema = new Schema({
+    creator: {
+        type: Schema.Types.ObjectId,
+        ref: 'USer',
+
+    },
+    prompt:{
+        type: String,
+        required: [true, 'Please enter a prompt'],
+
+    },
+    tag: {
+        type: String,
+        required: [true, 'Please enter a tag'],
+    }
+})
+
+const Prompt = models.Prompt || model('Prompt', promptSchema);
+
+export default Prompt;
